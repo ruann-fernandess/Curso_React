a2f1f3aff5265c0c911421d8000bb10c975b9b03
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-02-24 15:29:06 -0300

    Aula 3 - Manipulando JSX

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Ruan"
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
      <h1>Olá, Mundo!</h1>
      <p>Meu primeiro app</p>
+     <h2>Alterando o JSX</h2>
+     <p>Olá, {name}</p>
+     <p>Soma: 2 + 2 = {2+2}</p>
+     <img src={url} alt="Minha imagem 150x150" />
+     
     </div>
   );
 }
