bb5963490074c37e2ff4d778bb9ede619f52be34
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-09 15:27:45 -0300

    Aula 11 - Props em Eventos

--- src/components/Evento.jsx
+++ src/components/Evento.jsx
@@ -1,12 +1,20 @@
-function Evento({numero}){
+import Button from './evento/Button'
+
+function Evento(){
 
     function meuEvento(){
-        console.log(`Fui ativado, bebê ${numero}`)
+        console.log(`Ativando primeiro evento!`)
+    }
+
+    function segundoEvento(){
+        console.log(`Ativando segundo evento!`)
     }
 
     return(
         <div>
             <p>Clique para disparar um evento</p>
+            <Button event={meuEvento} text="Primeiro evento"/>
+            <Button event={segundoEvento} text="Segundo evento"/>
             <button onClick={meuEvento}>Ativar</button>
         </div>
     )
