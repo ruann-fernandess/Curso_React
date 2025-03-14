a021cc708ef73c1e3c1b7db502f1389fa16458f5
Ruan Fernandes Mendonça <ruanfernandes1242770@gmail.com>
2025-03-13 22:26:58 -0300

    Aula 14 - State Lift

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import SeuNome from './components/SeuNome';
+import Saudacao from './components/Saudacao';
+
+function App() {
+  const [nome, setNome] = useState()
+
+  return (
+    <div className="App">
+      <h1>Título da Aula</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>
+    </div>
+  );
+}
+
+export default App;
